90868cb93ff4a941ef7efabf729f83fcac528bcc
mabreydn <mabreydn@mail.uc.edu>
2025-08-03 10:49:22 -0400

    Created Next ViewModel
    Created GameListViewModel for the GameList content page. Installed the community toolkit MVVM but may not be used within project. Next is the AddGame page.

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,10 +1,11 @@
 ﻿// BaseViewModel.cs (in your ViewModels folder or a common folder)
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Gameromicon.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : ObservableObject
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
--- ViewModels/GameListViewModel.cs
+++ ViewModels/GameListViewModel.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Gameromicon.Classes;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Gameromicon.ViewModels
 {
-    internal class GameListViewModel
+    public partial class GameListViewModel : BaseViewModel
     {
+        
+        public string SearchQuery
+        {
+            get => searchQuery;
+            set => SetProperty(ref searchQuery, value);
+        }
+        private string searchQuery = string.Empty;
+
+        public ObservableCollection<Game> GameCollection
+        {
+            get => gameCollection;
+            set => SetProperty(ref gameCollection, value);
+        }
+        private ObservableCollection<Game> gameCollection;
+
+        public Game? SelectedGame
+        { get => selectedGame; 
+          set => SetProperty(ref selectedGame, value);
+        }
+        private Game? selectedGame;
+
+        public GameListViewModel()
+        {
+            GameCollection = new ObservableCollection<Game>()
+            {
+                new Game { ID = 1, Name = "The Legend of Zelda: Breath of the Wild", Barcode = "1234567890123", Boxart = "zelda_botw.jpg", CollectedDate = DateTime.Now, CiB = true, Condition = "New", Region = "NA", NumberOfCopies = 1, Description = "An open-world action-adventure game set in the kingdom of Hyrule.", ReleaseDate = new DateTime(2017, 3, 3), IsBeaten = false, Is100Completed = false, PublisherID = 1, SeriesID = 1, ProfileID = 1 },
+                new Game { ID = 2, Name = "Super Mario Odyssey", Barcode = "1234567890124", Boxart = "mario_odyssey.jpg", CollectedDate = DateTime.Now, CiB = true, Condition = "Like New", Region = "EU", NumberOfCopies = 1, Description = "A platform game featuring Mario in a 3D world.", ReleaseDate = new DateTime(2017, 10, 27), IsBeaten = false, Is100Completed = false, PublisherID = 1, SeriesID = 2, ProfileID = 1 }
+
+            };
+        }
+
+        [RelayCommand]
+        private Task Sort()
+        {
+            Debug.WriteLine("Sorting games...");
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private Task Filter()
+        {
+            Debug.WriteLine("Filtering games...");
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private Task AddGame()
+        {
+            Debug.WriteLine("Adding a new game...");
+            // Logic to add a new game
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task SelectGame(Game game)
+        {
+            if (game == null)
+            {
+                Debug.WriteLine("No game selected.");
+                return;
+            }
+            Debug.WriteLine($"Selected game: {game.Name}");
+            await Task.Delay(50);
+            selectedGame = null;
+        }
     }
 }
